cc53e67a22a492b77f8ba6aeabe9ac23b2c02861
Egor Kruglov <yegorkruglov@gmail.com>
2025-02-06 12:07:49 +0300

    Initial Commit

---
+++ FindMyFriends/Modules/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  FindMyFriends
+//
+//  Created by Egor Kruglov on 06.02.2025.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+}
+
+private extension ViewController {
+    
+    func setup() {
+        addSubviews()
+        setupSubviews()
+        makeConstraints()
+    }
+    
+    func addSubviews() {
+        
+    }
+    
+    func setupSubviews() {
+        view.backgroundColor = .systemGray6
+    }
+    
+    func makeConstraints() {
+        
+    }
+    
+}
+
